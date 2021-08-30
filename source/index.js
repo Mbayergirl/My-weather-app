df21adee408b4630352370359d6bb34ee352922d
Mbayergirl <87529738+Mbayergirl@users.noreply.github.com>
2021-08-30 11:13:49 +0100

    fixing the day and time issue

--- source/index.js
+++ source/index.js
@@ -1,26 +1,3 @@
-let now = new Date();
-let time = document.querySelector("#date");
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-time.innerHTML = `${day}, ${hours}:${minutes}`;
-
 function showCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
@@ -132,29 +109,6 @@ function showTemp(response) {
   sunriseElement.innerHTML = formatDate(response.data.sys.sunrise * 1000);
   let sunsetElement = document.querySelector("#sunset");
   sunsetElement.innerHTML = formatDateSunset(response.data.sys.sunset * 1000);
-
-  let now = new Date();
-  let time = document.querySelector("#date");
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  time.innerHTML = `${day}, ${hours}:${minutes}`;
 }
 
 function formatDay(timestamp) {
